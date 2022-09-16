8e1afe309d8f6eb578e0e4a6ade447a3901e5d79
Rugile <rugilestasionyte@gmail.com>
2022-09-16 09:47:44 +0300

    test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,10 +6,17 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+    public function __construct (){
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+        header('Access-Control-Allow-Headers: token, Content-Type');
+    }
+    
     public function second(){
-
+      
         return response()->json([
             'message' => 'Thank you.',
         ]);
     }
+
 }
