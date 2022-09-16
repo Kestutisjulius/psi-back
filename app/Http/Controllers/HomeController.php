a68514be3833db2bfb07ac2b9c49e0b7d2b00ab3
Kęstutis Skrebė <kestutis.julius@gmail.com>
2022-09-16 08:48:12 +0300

    homeController HEADERS

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,10 @@ class HomeController extends Controller
 {
     public function second(){
 
+
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATH, OPTIONS');
+        header('Access-Control-Allow-Headers: token, Content-Type');
         return response()->json([
             'message' => 'Thank you.',
         ]);
